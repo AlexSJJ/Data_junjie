c6e030b4781441dd8c1c6b0e9d86af48c2484669
Su jayce <jayce00@connect.hku.hk>
2021-07-12 22:20:22 +0800

    Initial commit

---
+++ generate_customers.bat
@@ -0,0 +1,2 @@
+REM python datagen_customer.py noOfCustomers randomSimulationSeed "configPath" >> "OPcsv"
+python datagen_customer.py 1000 4444 ".\profiles\main_config.json" >> ".\data\customers.csv"
---
+++ test2.py
@@ -0,0 +1,6 @@
+try:
+    m = 'profiles/main_config.json'
+    main = open(m, 'r').read()
+    print('success')
+except:
+    print(3)
